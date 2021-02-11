c99930e789c12c121eb5072d0aeed8ca96a044c4
Lucas Menezes <lucasm@lucasm.dev>
2021-02-11 18:45:05 -0300

    descriptions and footer

--- components/Footer.js
+++ components/Footer.js
@@ -11,7 +11,7 @@ function getYear() {
     <footer>
 
       <div class="container">
-        <p>Copyright {getYear()} Lucas Menezes | <Link href="/privacy"><a accessKey="6">Privacy</a></Link></p>
+        <p>Copyright {getYear()} Lucas Menezes <br/> <Link href="/privacy"><a accessKey="6">Privacy</a></Link></p>
       </div>
 		
     </footer>
